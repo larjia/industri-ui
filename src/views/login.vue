2a9a80fb680df5a4299295ec1fea9f77ebe2792f
larjia <yongzhen.jiang@outlook.com>
2020-03-07 19:32:05 +0800

    add user module in store

--- src/views/login.vue
+++ src/views/login.vue
@@ -43,8 +43,8 @@
             type="primary"
             style="width:100%;"
             @click.native.prevent="handleLogin">
-            <span v-if="!loading">登录</span>
-            <span v-else>登录中...</span>
+            <span v-if="!loading">登 录</span>
+            <span v-else>登 录 中...</span>
           </el-button>
         </el-form-item>
       </el-form>
