85dcb5a4a6d5aa2fd23ce0ec74614fd2bffae4bb
larjia <yongzhen.jiang@outlook.com>
2020-03-10 09:06:00 +0800

    navbar.vue

--- src/layout/components/Navbar.vue
+++ src/layout/components/Navbar.vue
@@ -1,5 +1,46 @@
 <template>
   <div class="navbar">
+    <hamburger 
+      id="hamburger-container"
+      :is-active="sidebar.opened" 
+      class="hamburger-container"
+      @toggleClick="toggleSideBar"
+    />
+
+    <breadcrumb 
+      id="breadcrumb-container"
+      class="breancrumb-conatiner"
+    />
+
+    <div class="right-menu">
+      <!-- <template v-if="device !== 'mobile'">
+      </template> -->
+      <el-dropdown 
+        class="avatar-container right-menu-item hover-effect" 
+        trigger="click"
+      >
+        <div 
+          class="avatar-wrapper"
+        >
+          <img 
+            :src="avatar" 
+            class="user-avatar"
+          >
+          <i class="el-icon-caret-bottom" />
+        </div>
+        <el-dropdown-menu slot="dropdown">
+          <router-link to="/user/profile">
+            <el-dropdown-item>个人中心</el-dropdown-item>
+          </router-link>
+          <!-- <el-dropdown-item>
+            <span @click="setting = true">布局设置</span>
+          </el-dropdown-item> -->
+        </el-dropdown-menu>
+        <el-dropdown-item divided>
+          <span @click="logout">退出登录</span>
+        </el-dropdown-item>
+      </el-dropdown>
+    </div>
   </div>
 </template>
 
@@ -37,7 +78,7 @@ export default {
       this.$store.dispatch('app/toggleSideBar')
     },
     async logout() {
-      this.$confirm('', '', {
+      this.$confirm('确定注销并退出系统吗?', '提示', {
         $confirmButtonText: '确定',
         cancelButtonText: '取消',
         type: 'warning'
@@ -55,9 +96,83 @@ export default {
 .navbar {
   height: 50px;
   overflow: hidden;
-  // background: #fff;
-  background: azure;
+  background: #fff;
+  // background: azure;
   position: relative;
   box-shadow: 0 1px 4px rgba(0,21,41,0.08);
+
+  .hamburger-container {
+    line-height: 46px;
+    height: 100%;
+    float: left;
+    cursor: pointer;
+    transition: background .3s;
+    -webkit-tap-highlight-color:transparent;
+
+    &:hover {
+      background: rbga(0, 0, 0, 0.025);
+    }
+  }
+
+  .breadcrumb-container {
+    float: left;
+  }
+
+  .errLog-container {
+    display: inline-block;
+    vertical-align: top;
+  }
+
+  .right-menu {
+    float: right;
+    height: 100%;
+    line-height: 50px;
+
+    &:focus {
+      outline: none;
+    }
+
+    .right-menu-item {
+      display: inline-block;
+      padding: 0 8px;
+      height: 100%;
+      font-size: 18px;
+      color: #5a5e66;
+      vertical-align: text-bottom;
+
+      &.hover-effect {
+        cursor: pointer;
+        transition: background .3s;
+
+        &:hover {
+          background: rgba(0, 0, 0, 0.025);
+        }
+      }
+    }
+
+    .avatar-container {
+      margin-right: 30px;
+
+      .avatar-wrapper {
+        margin-top: 5px;
+        position: relative;
+
+        .user-avatar {
+          cursor: pointer;
+          width: 40px;
+          height: 40px;
+          border-radius: 10px;
+        }
+
+        .el-icon-caret-bottom {
+          cursor: pointer;
+          position: absolute;
+          right: -20px;
+          top: 25px;
+          font-size: 12px;
+        }
+      }
+    }
+  }
 }
 </style>
