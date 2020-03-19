5d059024d97cca5f77cc39ffeb8e1a3f5d167de6
Jiang <JiangL@winkelmann-group.com>
2020-03-19 14:49:49 +0800

    resolve CORS issue

--- vue.config.js
+++ vue.config.js
@@ -33,7 +33,7 @@ module.exports = {
     proxy: {
       // detail: https://cli.vuejs.org/config/#devserver-proxy
       [process.env.VUE_APP_BASE_API]: {
-        target: `http://localhost:8082`,
+        target: `http://10.30.16.74:8082`,
         changeOrigin: true,
         pathRewrite: {
           ['^' + process.env.VUE_APP_BASE_API]: ''
