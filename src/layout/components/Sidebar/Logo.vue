79b198308c9308ad9ad797d8b1ccd3cd349f3745
larjia <yongzhen.jiang@outlook.com>
2020-03-25 21:25:37 +0800

    change logo and icon

--- src/layout/components/Sidebar/Logo.vue
+++ src/layout/components/Sidebar/Logo.vue
@@ -85,8 +85,10 @@ export default {
     width: 100%;
 
     & .sidebar-logo {
-      width: 32px;
-      height: 32px;
+      // width: 32px;
+      // height: 32px;
+      width: 25px;
+      height: 25px;
       vertical-align: middle;
       margin-right: 12px;
     }
