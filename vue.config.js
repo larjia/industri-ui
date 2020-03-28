d49fa858cf57857b7d36365b05e13dc7f97c2f26
larjia <yongzhen.jiang@outlook.com>
2020-03-28 16:35:55 +0800

    switch environment

--- vue.config.js
+++ vue.config.js
@@ -34,6 +34,7 @@ module.exports = {
       // detail: https://cli.vuejs.org/config/#devserver-proxy
       [process.env.VUE_APP_BASE_API]: {
         // target: `http://10.30.16.74:8082`,
+        // target: `http://47.103.91.203:8082`,
         target: `http://localhost:8082`,
         changeOrigin: true,
         pathRewrite: {
